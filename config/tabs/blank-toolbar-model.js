bc7f06cce3724293185cec86a99df4ffe321abde
Daniele Salvatore <daniele.salvatore@fao.org>
2016-03-25 16:28:30 +0100

    Blank toolbar configuration

--- config/tabs/blank-toolbar-model.js
+++ config/tabs/blank-toolbar-model.js
@@ -6,114 +6,92 @@ define(function () {
 
     return {
 
-/*        "range": {
+        "aggregation": {
 
             "selector": {
-                "id": "range",
-                "default": [126],
-                "config": { //specific ion.rangeSlider
-                    type: "double",
-                    min: 100,
-                    max: 200
-                },
-                "hideFooter": true
+                "id": "dropdown",
+                "source": [
+                    {"value": "aggregation_1", "label": "my aggregation fn 1"},
+                    {"value": "aggregation_2", "label": "my aggregation fn 2"},
+                    {"value": "aggregation_3", "label": "my aggregation fn 3"},
+                    {"value": "aggregation_4", "label": "my aggregation fn 4"},
+                    {"value": "aggregation_5", "label": "my aggregation fn 5"}
+                ], // Static data
+                "config": { //Selectize configuration
+                    "maxItems": 1
+                }
             },
 
-            "template": {
-                "hideHeader": true,
-                //"hideSwitch": true
+            "template" : {
+                "hideHeader" : true
             },
 
-            //dependencies with other selectors
-            "dependencies": {
-                //@ for special selection
-                //"@all": {id: "ensure_unset", event: "disable"} // obj, array of obj
-            },
+            "className" : "col-xs-6"
 
-            "format": {
-                //"output" : "codes", // codelist || time. if format is FENIX
-                //"uid" : "myCodelist", //override codelist uid config
-                //"version" : "myVersion", //override codelist version config
-                //"dimension" : "myDimension", //override dimension uid, default is the selector id
-            }
-        },*/
-
-        "donor": {
-
-            //"className" : "col-xs-6",
-
-            "cl": {
-                "uid": "crs_donors",
-                "version": "2016"
-            },
-
-            "selector": {
-                "id": "tree",
-                //"disabled" : true,
-                //"hideSelectAllButton": false,
-                //"source": [{"value": "myvalue", "label": "my custom label"}], // Static data
-                //"default": [1012],
-                //"hideFooter": true,
-                "hideSummary" : true
-            },
-
-            "template": {
-                "hideHeader": true
-            },
-
-            "dependencies": {
-                //"compare": {id: "focus", event: "select"} //obj or array of obj
-            },
-
-            "format": {
-                "dimension": "donorcode",
-                "type": "dynamic",
-                "process": '{"donorcode": { "codes":[{"uid": "{{uid}}", "version": "{{version}}", "codes": [{{{codes}}}] } ]}}'
-            },
-
-            "validation": {
-                //"mandatory" : true
-            }
         },
 
-        "delivery": {
-
-            "cl": {
-                "uid": "crs_channels",
-                "version": "2016",
-                "level": 3,
-                "levels": 3
-            },
+        "decimal_separator": {
 
             "selector": {
-                "id": "tree",
-                //"hideSelectAllButton": false,
-                //"disabled" : true
-                // "default": [44006],
-                //"hideFooter": true,
-                "hideSummary" : true
+                "id": "input",
+                "type": "radio",
+                "default" : ["dot"],
+                "source": [
+                    {"value": "dot", "label": "Dot"},
+                    {"value": "comma", "label": "Comma"}
+
+                ]
             },
 
-            "dependencies": {
-                //"compare": {id: "focus", event: "select"} //obj or array of obj
+
+            "template" : {
+                "hideHeader" : true
             },
 
-            "template": {
-                "hideHeader": true,
-                //"hideSwitch": true
+
+            "className" : "col-xs-6"
+
+        },
+
+        "thousand": {
+
+            "selector": {
+                "id": "input",
+                "type": "radio",
+                "default" : ["enable"],
+                "source": [
+                    {"value": "enable", "label": "Enable"},
+                    {"value": "disable", "label": "Disable"}
+                ]
             },
 
-            "format": {
-                "dimension": "channelcode",
-                "type": "dynamic",
-                "process": '{"channelcode": { "codes":[{"uid": "{{{uid}}}", "version": "{{version}}", "codes": [{{{codes}}}] } ]}}'
+
+            "template" : {
+                "hideHeader" : true
             },
 
-            "validation": {
-                //"mandatory" : true
-            }
+            "className" : "col-xs-6"
+
+        },
+
+        "decimal_digit": {
+
+            "selector": {
+                "id": "input",
+                "type": "number",
+                "source": [
+                    {"value": "2"}
+                ]
+            },
+
+            "template" : {
+                "hideHeader" : true
+            },
+
+
+            "className" : "col-xs-6"
+
         }
-
     }
 
 });
