245506e82b55e601b82e2605e5441aacd87c1449
stefanocudini <stefano.cudini@gmail.com>
2016-04-14 17:37:10 +0200

    config map tab

--- config/tabs/map-toolbar-model.js
+++ config/tabs/map-toolbar-model.js
@@ -10,147 +10,31 @@ define([
 
     return $.extend(true, {}, {
 
-        "sort": {
+        "maptoolbar": {
 
-            "selector": {
-                "id": "sortable",
-                "source": [
-                    {"value": "", "label": "", parent: 'AGG'}
-                ], // Static data
-                "config": { //SortableJS configuration
-                    //disabled: true
-                }
-            },
-
-            "template": {
-                //"hideHeader": true,
-                "hideSwitch": true,
-                "hideRemoveButton": true,
-                "title": "Sort dimension"
-            },
-
-            "className": "col-xs-6"
-
-        },
-
-        "aggregation": {
-
-            "selector": {
-                "id": "dropdown",
-                "source": [], // Static data
-                "config": { //Selectize configuration
-                    "maxItems": 1
-                }
-            },
-
-            "template": {
-                //"hideHeader": true,
-                "hideSwitch": true,
-                "hideRemoveButton": true,
-                "title": "Aggregation function"
-            },
-
-            "className": "col-xs-6"
-
-        },
-
-        "renderer": {
-
-            "selector": {
-                "id": "dropdown",
-                "source": [
-
-                    {"value": "table", "label": "Grid"},
-                    {"value": "chart", "label": "Chart"},
-
-
-                ], // Static data
-                "config": { //Selectize configuration
-                    "maxItems": 1
-                }
-            },
-
-            "template": {
-                //"hideHeader": true,
-                "hideSwitch": true,
-                "hideRemoveButton": true,
-                "title": "Renderer function"
-            },
-
-            "className": "col-xs-6"
-
-        },
-
-        "decimal_separator": {
-
-            "selector": {
-                "id": "input",
-                "type": "radio",
-                "default": ["dot"],
-                "source": [
-                    {"value": "dot", "label": "Dot"},
-                    {"value": "comma", "label": "Comma"}
-
-                ]
-            },
-
-            "template": {
-                //"hideHeader": true,
-                "hideSwitch": true,
-                "hideRemoveButton": true,
-                "title": "Decimal separator"
-            },
-
-
-            "className": "col-xs-6"
-
-        },
-
-        "thousand": {
-
-            "selector": {
-                "id": "input",
-                "type": "radio",
-                "default": ["enable"],
-                "source": [
-                    {"value": "enable", "label": "Enable"},
-                    {"value": "disable", "label": "Disable"}
-                ]
-            },
-
-            "template": {
-                //"hideHeader": true,
-                "hideSwitch": true,
-                "hideRemoveButton": true,
-                "title": "Thousand"
-            },
-
-            "className": "col-xs-6"
-
-        },
-
-        "decimal_digit": {
-
-            "selector": {
-                "id": "input",
-                "type": "number",
-                "source": [
-                    {"value": "2"}
+            selector: {
+                id: "sortable",
+                source : [
+                    {value: "1", label: "layer1"},
+                    {value: "2", label: "layer2"},
+                    {value: "3", label: "layer3"}
                 ],
-                "config" : {
-                    "min" : 0
+                config: {
+                    itemRender:  function (model) {
+
+                        var $el = $("<h1>[LAYER BOX " +model.label + "]</h1>");
+
+                        $el.on("click", function () {
+                            alert('Active layer')
+                        })
+
+                        return $el;
+                    }
                 }
             },
-
-            "template": {
-                //"hideHeader": true,
-                "hideSwitch": true,
-                "hideRemoveButton": true,
-                "title": "Decimal digit"
-            },
-
-            "className": "col-xs-6"
-
+            template: {
+                title: "Map Filter"
+            }
         }
 
     }, Shared)
