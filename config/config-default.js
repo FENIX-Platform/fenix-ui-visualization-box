e087ce4b78eb5f6484f3685740f2df43dd428736
deneb1 <deneb1ster@gmail.com>
2016-04-15 17:26:38 +0200

    .

--- config/config-default.js
+++ config/config-default.js
@@ -36,7 +36,7 @@ define(function () {
         },
         tabs: {
             //'blank': {type: 'simple', callback: 'once'},
-            //'table': {type: 'simple', callback: 'once'},
+            'table': {type: 'simple', callback: 'once'},
             'metadata': {type: 'simple', callback: 'once'},
             'filter': {type: 'simple', callback: 'once'},
             'map': {type: 'simple', callback: 'always'},
