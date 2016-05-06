2191f1a75b7961983049555320ba24330602f95d
stefanocudini <stefano.cudini@gmail.com>
2016-05-06 14:30:48 +0200

    fix olap path in map test

--- test/map.js
+++ test/map.js
@@ -29,8 +29,7 @@ require.config({
         metadataEditorPaths : pathProjectRoot + 'submodules/fenix-ui-metadata-editor/js/paths',
         metadataViewerPaths : pathProjectRoot + 'submodules/fenix-ui-metadata-viewer/src/js/paths',
         catalogPaths : pathProjectRoot + 'submodules/fenix-ui-catalog/js/paths',
-        dataManagementPaths : pathProjectRoot + 'submodules/fenix-ui-data-management/src/js/paths',
-
+        dataManagementPaths : pathProjectRoot + 'submodules/fenix-ui-data-management/src/js/paths'
     }
 });
 
@@ -60,7 +59,7 @@ require([
     filterConfig.baseUrl = submodules_path + 'fenix-ui-filter/src/js';
 
     var olapConfig = Olap;
-    olapConfig.baseUrl = submodules_path + 'fenix-ui-olap/js';
+    olapConfig.baseUrl = submodules_path + 'fenix-ui-olap/src/js';
 	
     var metadataViewerConfig = MetadataViewer;
     metadataViewerConfig.baseUrl = submodules_path + 'fenix-ui-metadata-viewer/src/js';
