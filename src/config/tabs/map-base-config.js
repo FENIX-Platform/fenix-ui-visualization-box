4bc52cd47ddf3ea3c9d228b01d53688cf120d2e7
stefanocudini <stefano.cudini@gmail.com>
2016-12-15 16:30:32 +0100

    update map creator

--- src/config/tabs/map-base-config.js
+++ src/config/tabs/map-base-config.js
@@ -14,19 +14,22 @@ define({
             title_fr: "CartoDB light",
             url: 'http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png',
             subdomains: 'abcd',
-            maxZoom: 19
+            maxZoom: 19,
+            opacity: 1
         },
         osm: {
             //url: "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
             url: "http://{s}.tiles.wmflabs.org/osm-no-labels/{z}/{x}/{y}.png",
             title_en: "Openstreetmap",
             title_fr: "Openstreetmap",
-            maxZoom: 16
+            maxZoom: 16,
+            opacity: 1            
         },
         world_imagery: {
             url: "http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
             title_en: "World Imagery",
-            title_fr: "World Imagery"
+            title_fr: "World Imagery",
+            opacity:1
         }
     },
     legendOptions: {
